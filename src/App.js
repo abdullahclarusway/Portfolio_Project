12532a46fb2eccec31bbbebab324b04eb963e25e
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-23 23:43:44 +0100

    Initiliaze project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div >
+      <h1>Let's build a portfolio project</h1>
+    </div>
+  );
+}
+
+export default App;
